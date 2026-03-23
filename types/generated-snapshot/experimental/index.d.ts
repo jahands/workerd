77f9e20274b20c3365b7a873156d9c8f5fd57ced
Pratham <pkhanna@cloudflare.com>
2026-03-23 16:23:17 +0000

    custom limits for dynamic workers

--- types/generated-snapshot/experimental/index.d.ts
+++ types/generated-snapshot/experimental/index.d.ts
@@ -4209,6 +4209,7 @@ interface WorkerStub {
 }
 interface WorkerStubEntrypointOptions {
   props?: any;
+  limits?: workerdResourceLimits;
 }
 interface WorkerLoader {
   get(
@@ -4230,12 +4231,17 @@ interface WorkerLoaderWorkerCode {
   compatibilityDate: string;
   compatibilityFlags?: string[];
   allowExperimental?: boolean;
+  limits?: workerdResourceLimits;
   mainModule: string;
   modules: Record<string, WorkerLoaderModule | string>;
   env?: any;
   globalOutbound?: Fetcher | null;
   tails?: Fetcher[];
   streamingTails?: Fetcher[];
+}
+interface workerdResourceLimits {
+  cpuMs?: number;
+  subRequests?: number;
 }
 /**
  * The Workers runtime supports a subset of the Performance API, used to measure timing and performance,
